f52eedab98f50be9d2587e813463f58e9d9e3829
mvarela323 <137104958+mvarela323@users.noreply.github.com>
2023-10-04 18:05:09 -0700

    Add files via upload

---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'reactstrap'; 
+import Partner from './Partner';
+import { selectAllPartners } from './partnersSlice';
+
+const PartnersList = () => {
+  const partners = useSelector(selectAllPartners);
+
+  return (
+    <Col className='mt-4'>
+      <Row>
+        {partners.map((partner) => (
+          <div className='d-flex mb-5' key={partner.id}>
+            <Partner partner={partner} />
+          </div>
+        ))}
+      </Row>
+    </Col>
+  );
+};
+
+export default PartnersList;
---
+++ src/features/partners/partnersSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { PARTNERS } from '../../app/shared/PARTNERS'; // 
+//import { startTrackValue } from '@testing-library/user-event/dist//document';
+
+const initialState = {
+  partnersArray: PARTNERS
+};
+
+const partnersSlice = createSlice({
+    name: 'partners',
+    initialState
+});
+
+export const partnersReducer = partnersSlice.reducer;
+
+export const selectAllPartners = (state) => {
+  return state.partners.partnersArray;
+};
+
+
+export const selectFeaturedPartner = (state) => {
+  return state.partners.partnersArray.find((partner) => partner.featured);
+};
+
